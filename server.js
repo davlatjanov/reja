34db9ba579bbf9bf3e182a594d49b2d7202a29c3
davlatjanov <ndavlatjonov@gmail.com>
2025-09-16 13:56:10 +0900

    feat: EJS frameworkida publishing DONE

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const { setDefaultResultOrder } = require("dns");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -23,7 +33,9 @@ app.post("/create-item", (req, res) => {
 app.get("/", function (req, res) {
   res.render("harid");
 });
-
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
 const server = http.createServer(app);
 let PORT = 3004;
 
